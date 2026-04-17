3b532ca0fffca88032854d58eb6fc4c763ca9f76
jonassteinraths <jonassteinraths@gmail.com>
2026-04-17 10:56:06 +0000

    dsds

---
+++ Fortbewegungsmittel/Interfaces/Beschleunigbar.java
@@ -0,0 +1,3 @@
+public interface Beschleunigbar {
+    public void beschleunigen(double b);
+}
--- Fortbewegungsmittel/Straßenfahrzeug.java
+++ Fortbewegungsmittel/Straßenfahrzeug.java
@@ -1,6 +1,6 @@
 public abstract class Straßenfahrzeug extends Landfahrzeug {
     protected double bodenfreiheit;
-    public Straßenfahrzeuge(double g, int a, double bodenfreiheit) {
+    public Straßenfahrzeug(double g, int a, double bodenfreiheit) {
         super(g, a);
         this.bodenfreiheit = bodenfreiheit;
     
