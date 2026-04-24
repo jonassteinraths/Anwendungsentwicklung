ad7d1620b8ad75818d6797a0d637f32878276669
jonassteinraths <jonassteinraths@gmail.com>
2026-04-24 11:20:42 +0000

    exception weiter

--- Exceptions/Main.java
+++ Exceptions/Main.java
@@ -1,20 +1,26 @@
 
 public class Main {
     public static void main(String[] args) {
+        int[] x=new int[3]
+        x[0]=17;
+        x[1]=123;
+        x[2]=42;
 
         try {
-            int[] a = new int[5];
-            a[2] = 10;
+            System.out.println(x[2]);
+            System.out.println(x[3]);
+            System.out.println(x[0]);
+            int[] v=new int[-5]
+        }
+        catch(NegativeArraySizeException f){
+            System.out.println("Fehler: Arraygröße darf niemals Negativ sein. ");
+            f.printStackTrace();
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Indexfehler");
+            e.printStackTrace();
+        }
 
-            //fehler einbauen
-            int[] b = new int[-5];
-
-        }
-        catch(NegativeArraySizeException e){
-            System.out.println("Fehler: Arraygröße darf niemals Negativ sein. ")
-        }
-        finally {
-            
-        }
+        
     }
 }
