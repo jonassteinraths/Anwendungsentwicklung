2afe7dd1769b7c88d745ee09abe6b19284d1070a
jonassteinraths <jonassteinraths@gmail.com>
2026-05-29 10:30:37 +0000

    ..

---
+++ collections/JCFTester.java
@@ -0,0 +1,18 @@
+public class JCFTester {
+    public static void main(String[] args){
+        Integer i = 123;
+
+        Vector<String> ohneTyp = new Vector<String>();
+        ohneTyp.add(s);
+        ohneTyp.add(new String("123"));
+        Iterator it = ohneTyp.iterator();
+
+        while(it.hasNext())
+        {
+            String inhalt =(String) it.next();
+            System.out.println("Inhalt +"Inhalt)
+        }
+    }
+}
+
+
